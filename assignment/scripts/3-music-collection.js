0bf3874180047042f211a0f6aa928e617b434a2f
LazyDotExe <papaporo@proton.me>
2023-07-06 16:15:43 -0500

    Created a function that will push a new music collection into the collection array.

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,5 +1,19 @@
 console.log('***** Music Collection *****')
 
-let collection = [];
+const collection = [];
+
+
+
+function addToCollection(title, artist, yearPublished) {
+    collection.push({
+        "title": title,
+        "artist": artist,
+        "yearPublished": yearPublished
+    });
+}
+
+addToCollection('Badfish', 'sublime', 1990)
+addToCollection('One', 'Metallica', 1990)
+console.log(collection);
 
 
