5fe5c110e0f61f248886825450560d4d82575dbe
LazyDotExe <papaporo@proton.me>
2023-07-06 18:25:42 -0500

    created a 'showCollection' function that will log all of the objects properties on a new line.

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -13,21 +13,7 @@ function addToCollection(title, artist, yearPublished) { // a function that take
     return collection[collection.length - 1] //this line will return the last object you have "pushed" to the array.
 }
 //.................................................................................................................................................................................
-function showCollection(Array) {
-    for (let i = 0; i < Array.length; 1++)
-    console.log(Array.length);
-    console.log()
 
-}
-
-
-
-
-
-
-
-
-//.................................................................................................................................................................................
 
 addToCollection('Badfish', 'sublime', 1991)
 addToCollection('One', 'Metallica', 1988)
@@ -35,8 +21,22 @@ addToCollection('Hotel California', 'Eagles', 1976)
 addToCollection('Yellow Ledbetter', 'Pearl Jam', 2000)
 // let record5 = addToCollection('My body', 'Young the Giant', 2010)
 addToCollection('Sex on fire', 'Kings of leon', 2008)
-console.log(collection);
+// console.log(collection);
 // console.log(record5)
+
+//.................................................................................................................................................................................
+
+
+function showCollection(arr) { //function 'showCollection' that takes one perameter.
+    for (let i = 0; i < arr.length; i++) { //if loop that starts at 0 and then increases by one while its less then arr.length
+    console.log(`${arr[i].title} by ${arr[i].artist}, published in ${arr[i].yearPublished}`); //this uses the i in the for loop and console logs each property that makes up the [i] index 
+    }
+    return console.log(arr.length); //this line will just return the length of whatever property (array) you use
+}
+
+showCollection(collection);
+
+//.................................................................................................................................................................................
 
 
 
