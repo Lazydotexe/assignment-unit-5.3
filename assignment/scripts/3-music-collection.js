1cc8377e7260faad21d2167427decc419ff737ee
LazyDotExe <papaporo@proton.me>
2023-07-08 04:01:24 -0500

    Working on stretch goals

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -4,11 +4,12 @@ const collection = []; //sets 'collection' to an empty array
 
 
 
-function addToCollection(title, artist, yearPublished) { // a function that takes 3 properties; title, artist, yearPublished.
+function addToCollection(title, artist, yearPublished, name = "Add song", diration = 'Add length') { // a function that takes 3 properties; title, artist, yearPublished.
     collection.push({ //this line will 'push' whatever follows to the end of the empty array 'collection'
         "title": title,
         "artist": artist,// these three lines have 3 different properties that will use whatever value you plugged into the function property.
-        "yearPublished": yearPublished
+        "yearPublished": yearPublished,
+        "tracks": [name, diration]
     });
     return collection[collection.length - 1]; //this line will return the last object you have "pushed" to the array.
 }
@@ -32,13 +33,13 @@ function addToCollection(title, artist, yearPublished) { // a function that take
 //.................................................................................................................................................................................
 
 
-console.log(addToCollection("Jah won't pay the bills", 'Sublime', 1991));
-console.log(addToCollection('...And Justice for All', 'Metallica', 1988));
-console.log(addToCollection('Hotel California', 'Eagles', 1976));
-console.log(addToCollection('Ten', 'Pearl Jam', 2000));
-console.log(addToCollection('Only by the Night', 'Kings of leon', 2008));
-console.log(addToCollection('Young the Giant', 'Young the Giant', 2010));
-console.log(addToCollection('Metallica', 'Metallica', 1991));
+console.log(addToCollection("Jah won't pay the bills", 'Sublime', 1991, "Badfish", "3:05"));
+console.log(addToCollection('...And Justice for All', 'Metallica', 1988, "One", "7:26"));
+console.log(addToCollection('Hotel California', 'Eagles', 1976, 'Hotel California', '6:31'));
+console.log(addToCollection('Ten', 'Pearl Jam', 2000, 'Yellow Ledbetter', '5:03'));
+console.log(addToCollection('Only by the Night', 'Kings of leon', 2008, 'Sex on fire', '3:23'));
+console.log(addToCollection('Young the Giant', 'Young the Giant', 2010, 'My Body', '4:04'));
+console.log(addToCollection('Metallica', 'Metallica', 1991, 'My Friend Misery', '6:49'));
 console.log(collection);
 
 
@@ -48,6 +49,7 @@ console.log(collection);
 function showCollection(arr) { //function 'showCollection' that takes one perameter.
     for (let i = 0; i < arr.length; i++) { //if loop that starts at 0 and then increases by one while its less then arr.length
     console.log(`${arr[i].title} by ${arr[i].artist}, published in ${arr[i].yearPublished}`); //this uses the i in the for loop and console logs each property that makes up the [i] index 
+    console.log(`1.${arr[i].tracks[0]}: ${arr[i].tracks[1]}`)
     }
     return console.log(arr.length); //this line will just return the length of whatever property (array) you use
 }
@@ -85,11 +87,14 @@ function findByArtist(artist) { // sets a function that takes the property 'arti
 
 let newAlbum = [];//sets empty array
 
-function search(artist, year) {//function that takes two perameters
+function search(artist, year, trackName) {//function that takes two perameters
    newAlbum = [];//resets the array to empty when its reused
   
     for (let i = 0; i < collection.length; i++) {//iterates through the collection array
-      if (collection[i].artist === artist && collection[i].yearPublished === year) {//is [i].artist === artist AND is [i].yearPublished === year
+      if (collection[i].tracks[0] === artist || year || trackName) {
+        newAlbum.push(collection[i].tracks)
+        return newAlbum;
+      } else if (collection[i].artist === artist && collection[i].yearPublished === year) {//is [i].artist === artist AND is [i].yearPublished === year
         newAlbum.push(collection[i].title); //if yes then push [i].title to empty array
       } else if (collection[i].yearPublished === artist) {//if only a year is entered then it takes the 'artist' perameter spot and is compared to [i].yearPublished
         newAlbum.push(collection[i].title);//if yes then push [i].title for whatever year entered
@@ -103,9 +108,9 @@ function search(artist, year) {//function that takes two perameters
     return newAlbum;
   }
 
-console.log(search('Metallica'));
-console.log(search('Eagles', 1976));
-console.log(search(1991));
+// console.log(search('Metallica'));
+// console.log(search('Eagles', 1976));
+// console.log(search('Badfish'));
 
 
 
